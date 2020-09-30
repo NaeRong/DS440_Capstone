28100ba0dcfce6f3d2258c2ec3d50129eb2bae6c
Nadia Dimitrova <nadimitrova26@gmail.com>
2020-09-30 12:23:46 -0400

    bootstrap updated to work with Mac OS

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,8 +6,21 @@
 # This can mean packages, software language versions, Git submodules, etc.
 # The goal is to make sure all required dependencies are installed.
 
-# unzip to extract zip archives
-apt install unzip
+# Install based on OS
+# https://stackoverflow.com/a/3466183/363829
+case "$(uname -s)" in
+   Darwin)
+      # Do something under Mac OS X platform
+    echo "Kernal = Mac OS X, using brew install wget"
+    brew install wget
+     ;;
+
+   Linux)
+       # Do something under GNU/Linux platform
+    echo "Kernal = Linux, using apt install unzip"
+    apt install unzip
+     ;;
+esac
 
 ## Check for $FLOOD_ANALYSIS_CORE
 # https://stackoverflow.com/q/3601515
